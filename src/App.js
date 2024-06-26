ac9136181684c3e3233f5cfeffcf2b58f3d5a4ea
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-06-26 16:38:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,27 +13,36 @@ function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
   // adding days to date
   const date = new Date();
   date.setDate(date.getDate() + count);
 
   return (
     <div>
-      <div className="counter">
-        <button className="button" onClick={() => setStep((s) => s - 1)}>
-          -
-        </button>
-        <p>Step: {step}</p>
-        <button className="button" onClick={() => setStep((s) => s + 1)}>
-          +
-        </button>
-      </div>
+      <input
+        className="text-input"
+        type="range"
+        min="0"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <span>{step}</span>
 
       <div className="counter">
         <button className="button" onClick={() => setCount((c) => c - step)}>
           -
         </button>
-        <p>Count: {count}</p>
+        <input
+          className="text-input"
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button className="button" onClick={() => setCount((c) => c + step)}>
           +
         </button>
@@ -43,6 +52,14 @@ function Counter() {
       <span>{count > 0 ? `${count} days from today is ` : ""}</span>
       <span>{count < 0 ? `${Math.abs(count)} days ago was ` : ""}</span>
       <span>{date.toDateString()}</span>
+      <div>
+        <button
+          className={step === 1 && count === 0 ? "hidden" : "reset-button"}
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
